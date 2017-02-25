5af821401e380564588de55238abd61922c75fc3
HienNguyen711 <hienminhnguyen711@gmail.com>
2017-02-25 16:52:49 +0200

    index.jade :hamster:

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+'use strict';
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -8,7 +9,16 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
+//mockApi.json
+var api = require('./api/mockApi.json');
+//iterate array from object
+var apiArray = Object.keys(api).map(function(value){
+  return api[value]
+});
+
+
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,10 +30,11 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));//serve static files
 
 app.use('/', index);
 app.use('/users', users);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -43,4 +54,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+
+app.listen(3000,function(){
+  console.log('Running on port 3000');
+});
